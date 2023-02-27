15513bbb3d56f7cfff9e7f89342e57ec97e28627
Yannik Ache Eicher <ache@bbit.io>
2023-02-27 09:56:02 +0100

    feat(web): implemented permission check

--- src/web.ts
+++ src/web.ts
@@ -14,38 +14,43 @@ import {
 } from './definitions';
 
 export class BarcodeScannerWeb extends WebPlugin implements BarcodeScannerPlugin {
-  private formats: number[] = [];
-  private controls: IScannerControls | null = null;
-  private torchState = false;
-  private video: HTMLVideoElement | null = null;
-  private options: ScanOptions | null = null;
+  private _formats: number[] = [];
+  private _controls: IScannerControls | null = null;
+  private _torchState = false;
+  private _video: HTMLVideoElement | null = null;
+  private _options: ScanOptions | null = null;
+  private _backgroundColor: string | null = null;
 
   async prepare(): Promise<void> {
-    throw this.unimplemented('Not implemented on web.');
+    await this._getVideoElement();
+    return;
   }
 
   async hideBackground(): Promise<void> {
-    throw this.unimplemented('Not implemented on web.');
+    this._backgroundColor = document.documentElement.style.backgroundColor;
+    document.documentElement.style.backgroundColor = 'transparent';
+    return;
   }
 
   async showBackground(): Promise<void> {
-    throw this.unimplemented('Not implemented on web.');
+    document.documentElement.style.backgroundColor = this._backgroundColor || '';
+    return;
   }
 
   async startScan(_options: ScanOptions): Promise<ScanResult> {
-    this.options = _options;
-    this.formats = [];
+    this._options = _options;
+    this._formats = [];
     _options.targetedFormats?.forEach((format) => {
       const formatIndex = Object.keys(BarcodeFormat).indexOf(format);
       if (formatIndex >= 0) {
-        this.formats.push(0);
+        this._formats.push(0);
       } else {
-        console.log(format + ' not supported on web');
+        console.error(format, 'is not supported on web');
       }
     });
-    const video = await this.getVideoElement();
+    const video = await this._getVideoElement();
     if (video) {
-      return this.getFirstResultFromReader();
+      return await this._getFirstResultFromReader();
     } else {
       throw this.unavailable('Missing video element');
     }
@@ -56,74 +61,105 @@ export class BarcodeScannerWeb extends WebPlugin implements BarcodeScannerPlugin
   }
 
   async pauseScanning(): Promise<void> {
-    if (this.controls) {
-      this.controls.stop();
-      this.controls = null;
+    if (this._controls) {
+      this._controls.stop();
+      this._controls = null;
     }
   }
 
   async resumeScanning(): Promise<void> {
-    this.getFirstResultFromReader();
+    this._getFirstResultFromReader();
   }
 
   async stopScan(_options?: StopScanOptions): Promise<void> {
-    this.stop();
-    if (this.controls) {
-      this.controls.stop();
-      this.controls = null;
+    this._stop();
+    if (this._controls) {
+      this._controls.stop();
+      this._controls = null;
     }
   }
 
   async checkPermission(_options: CheckPermissionOptions): Promise<CheckPermissionResult> {
-    throw this.unimplemented('Not implemented on web.');
+    if (typeof navigator === 'undefined' || !navigator.permissions) {
+      throw this.unavailable('Permissions API not available in this browser');
+    }
+
+    try {
+      // https://developer.mozilla.org/en-US/docs/Web/API/Permissions/query
+      // the specific permissions that are supported varies among browsers that implement the
+      // permissions API, so we need a try/catch in case 'camera' is invalid
+      const permission = await window.navigator.permissions.query({
+        name: 'camera' as any,
+      });
+      if (permission.state === 'prompt') {
+        return {
+          neverAsked: true,
+        };
+      }
+      if (permission.state === 'denied') {
+        return {
+          denied: true,
+        };
+      }
+      if (permission.state === 'granted') {
+        return {
+          granted: true,
+        };
+      }
+      return {
+        unknown: true,
+      };
+    } catch {
+      throw this.unavailable('Camera permissions are not available in this browser');
+    }
   }
 
   async openAppSettings(): Promise<void> {
-    throw this.unimplemented('Not implemented on web.');
+    throw this.unavailable('App settings are not available in this browser');
   }
 
   async disableTorch(): Promise<void> {
-    if (this.controls && this.controls.switchTorch) {
-      this.controls.switchTorch(false);
-      this.torchState = false;
+    if (this._controls && this._controls.switchTorch) {
+      this._controls.switchTorch(false);
+      this._torchState = false;
     }
   }
 
   async enableTorch(): Promise<void> {
-    if (this.controls && this.controls.switchTorch) {
-      this.controls.switchTorch(true);
-      this.torchState = true;
+    if (this._controls && this._controls.switchTorch) {
+      this._controls.switchTorch(true);
+      this._torchState = true;
     }
   }
 
   async toggleTorch(): Promise<void> {
-    if (this.controls && this.controls.switchTorch) {
-      this.controls.switchTorch(true);
+    if (this._controls && this._controls.switchTorch) {
+      this._controls.switchTorch(true);
     }
   }
 
   async getTorchState(): Promise<TorchStateResult> {
-    return { isEnabled: this.torchState };
+    return { isEnabled: this._torchState };
   }
 
-  private async getVideoElement() {
-    if (!this.video) {
-      await this.startVideo();
+  private async _getVideoElement() {
+    if (!this._video) {
+      await this._startVideo();
     }
-    return this.video;
+    return this._video;
   }
 
-  private async getFirstResultFromReader() {
-    const videoElement = await this.getVideoElement();
+  private async _getFirstResultFromReader() {
+    const videoElement = await this._getVideoElement();
     return new Promise<ScanResult>(async (resolve) => {
       if (videoElement) {
         let hints;
-        if (this.formats.length) {
+        if (this._formats.length) {
           hints = new Map();
-          hints.set(DecodeHintType.POSSIBLE_FORMATS, this.formats);
+          hints.set(DecodeHintType.POSSIBLE_FORMATS, this._formats);
         }
         const reader = new BrowserQRCodeReader(hints);
-        this.controls = await reader.decodeFromVideoElement(videoElement, (result, error, controls) => {
+        this._controls = await reader.decodeFromVideoElement(videoElement, (result, error, controls) => {
           if (!error && result) {
             resolve({
               hasContent: !!result.getText(),
@@ -131,8 +167,8 @@ export class BarcodeScannerWeb extends WebPlugin implements BarcodeScannerPlugin
               format: result.getBarcodeFormat().toString(),
             });
             controls.stop();
-            this.controls = null;
-            this.stop();
+            this._controls = null;
+            this._stop();
           }
           if (error && error.message) {
             console.error(error.message);
@@ -142,7 +178,7 @@ export class BarcodeScannerWeb extends WebPlugin implements BarcodeScannerPlugin
     });
   }
 
-  private async startVideo(): Promise<{}> {
+  private async _startVideo(): Promise<{}> {
     return new Promise(async (resolve, reject) => {
       await navigator.mediaDevices
         .getUserMedia({
@@ -166,16 +202,16 @@ export class BarcodeScannerWeb extends WebPlugin implements BarcodeScannerPlugin
           'style',
           'position:absolute; top: 0; left: 0; width:100%; height: 100%; background-color: black;'
         );
-        this.video = document.createElement('video');
-        this.video.id = 'video';
+        this._video = document.createElement('video');
+        this._video.id = 'video';
         // Don't flip video feed if camera is rear facing
-        if (this.options?.cameraDirection !== CameraDirection.BACK) {
-          this.video.setAttribute(
+        if (this._options?.cameraDirection !== CameraDirection.BACK) {
+          this._video.setAttribute(
             'style',
             '-webkit-transform: scaleX(-1); transform: scaleX(-1); width:100%; height: 100%;'
           );
         } else {
-          this.video.setAttribute('style', 'width:100%; height: 100%;');
+          this._video.setAttribute('style', 'width:100%; height: 100%;');
         }
 
         const userAgent = navigator.userAgent.toLowerCase();
@@ -185,12 +221,12 @@ export class BarcodeScannerWeb extends WebPlugin implements BarcodeScannerPlugin
         // Without these attributes this.video.play() will throw a NotAllowedError
         // https://developer.apple.com/documentation/webkit/delivering_video_content_for_safari
         if (isSafari) {
-          this.video.setAttribute('autoplay', 'true');
-          this.video.setAttribute('muted', 'true');
-          this.video.setAttribute('playsinline', 'true');
+          this._video.setAttribute('autoplay', 'true');
+          this._video.setAttribute('muted', 'true');
+          this._video.setAttribute('playsinline', 'true');
         }
 
-        parent.appendChild(this.video);
+        parent.appendChild(this._video);
         body.appendChild(parent);
 
         if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -201,9 +237,9 @@ export class BarcodeScannerWeb extends WebPlugin implements BarcodeScannerPlugin
           navigator.mediaDevices.getUserMedia(constraints).then(
             (stream) => {
               //video.src = window.URL.createObjectURL(stream);
-              if (this.video) {
-                this.video.srcObject = stream;
-                this.video.play();
+              if (this._video) {
+                this._video.srcObject = stream;
+                this._video.play();
               }
               resolve({});
             },
@@ -217,19 +253,20 @@ export class BarcodeScannerWeb extends WebPlugin implements BarcodeScannerPlugin
       }
     });
   }
-  private async stop(): Promise<any> {
-    if (this.video) {
-      this.video.pause();
 
-      const st: any = this.video.srcObject;
+  private async _stop(): Promise<any> {
+    if (this._video) {
+      this._video.pause();
+
+      const st: any = this._video.srcObject;
       const tracks = st.getTracks();
 
       for (var i = 0; i < tracks.length; i++) {
         var track = tracks[i];
         track.stop();
       }
-      this.video.parentElement?.remove();
-      this.video = null;
+      this._video.parentElement?.remove();
+      this._video = null;
     }
   }
 }
